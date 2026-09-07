154648f2ddcbf78d7c72376c526953b58f28c2f3
Leonardo <noreply@leoflo.me>
2026-09-07 08:41:45 +0200

    shell: load script per variabili di ambiente

--- modules/otis/programs/shell.nix
+++ modules/otis/programs/shell.nix
@@ -4,9 +4,22 @@
       enable = true;
       vteIntegration = true;
 
+      interactiveShellInit = ''
+      secrets_dir="$HOME/.config/secrets"
+
+      if [[ -d "$secrets_dir" ]]; then
+        set -a
+        for env_file in "$secrets_dir"/*.env; do
+          test -f "$env_file" || continue
+          source "$env_file"
+        done
+        set +a
+      fi
+      '';
+
       promptInit = ''
       export PS1='\[$(tput bold)$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 6)\]\h \[$(tput setaf 5)\]\w\[$(tput setaf 1)\]]\[$(tput sgr0)\]\$ '
-        '';
+      '';
 
       shellAliases = {
         "l" = "ls -lh";
