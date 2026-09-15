9fe3408ce2fd48fd9f5d417921982afe94e4d8e6
Leonardo <noreply@leoflo.me>
2026-09-15 12:48:17 +0200

    hyprland: monitor configs

--- modules/otis/gui/hyprland/monitors.lua
+++ modules/otis/gui/hyprland/monitors.lua
@@ -1,6 +1,13 @@
 hl.monitor({
-   output = "",
-   mode = "preferred",
-   position = "auto",
-   scale = "auto",
+   output = "eDP-1",
+   mode = "1920x1080@60",
+   position = "0x0",
+   scale = "1.0",
+})
+
+hl.monitor({
+   output = "HDMI-A-3",
+   mode = "1920x1080@75",
+   position = "0x0",
+   scale = "1.0",
 })
